384dc750d12bfab0793082eaa0090322139299e7
zuldare <mishasalnikov@gmail.com>
2024-04-14 02:47:43 +0200

    add more versions support

--- facebook/__init__.py
+++ facebook/__init__.py
@@ -41,7 +41,7 @@ __version__ = version.__version__
 FACEBOOK_GRAPH_URL = "https://graph.facebook.com/"
 FACEBOOK_WWW_URL = "https://www.facebook.com/"
 FACEBOOK_OAUTH_DIALOG_PATH = "dialog/oauth?"
-VALID_API_VERSIONS = ["3.1", "3.2", "3.3", "4.0", "5.0", "6.0", "7.0", "8.0", "8.2", "9.0", "10.0", "11.0", "12.0"]
+VALID_API_VERSIONS = ["3.1", "3.2", "3.3", "4.0", "5.0", "6.0", "7.0", "8.0", "8.2", "9.0", "10.0", "11.0", "12.0", "13.0", "14.0", "15.0", "16.0", "17.0", "18.0", "19.0", "20.0", "21.0", "22.0", "23.0", "24.0", "25.0", "26.0", "27.0"]
 VALID_SEARCH_TYPES = ["place", "placetopic"]
 
 
